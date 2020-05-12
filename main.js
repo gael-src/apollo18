889e93b1f073b8e03c75e7985b91d4f534e0cbf5
gael-src <gaelfr85@gmail.com>
2020-05-12 12:31:43 +0200

    commit

--- main.js
+++ main.js
@@ -3,59 +3,59 @@ const employees = [
     name: "Vadim",
     email: "vadim@apollo18.io",
     image:
-      "https://avatars2.githubusercontent.com/u/55791337?s=400&u=d2c9d80449cb05ab7ae3f6f655f7040e9aaf0015&v=4",
+      "./assets/vadim.jpeg",
     hobbies: ["code", "bmw", "work"],
   },
   {
     name: "Mariusz",
     email: "mariusz@apollo18.io",
     image:
-      "https://avatars2.githubusercontent.com/u/50579679?s=400&u=fed1dbed99f748599dacf9260ebdbc7d666df50f&v=4",
+      "./assets/mariusz.jpeg",
     hobbies: ["code", "painting", "design"],
   },
   {
     name: "Niels",
     email: "niels@apollo18.io",
     image:
-      "https://avatars1.githubusercontent.com/u/12913491?s=400&u=b311621271ea4c0a8e197eaeb5a5582c426ab2e8&v=4",
+      "./assets/niels.jpeg",
     hobbies: ["code", "music", "master"],
   },
   {
     name: "Pawel",
     email: "pawel@apollo18.io",
     image:
-      "https://avatars1.githubusercontent.com/u/50579605?s=400&u=5d1af461ffea1f90cea292cd60c498bcc6622c07&v=4",
+      "./assets/pawel.jpeg",
     hobbies: ["code", "cat", "done"],
   },
   {
     name: "Marco",
     email: "marco@apollo18.io",
-    image: "https://avatars3.githubusercontent.com/u/55787638?s=400&v=4",
+    image: "./assets/marco.jpeg",
     hobbies: ["code", "twitch", "baby"],
   },
   {
     name: "Andreas",
     email: "andreas@apollo18.io",
-    image: "https://avatars3.githubusercontent.com/u/50579748?s=400&v=4",
+    image: "./assets/andreas.jpeg",
     hobbies: ["code", "rap", "beer"],
   },
   {
     name: "Michael",
     email: "michael@apollo18.io",
     image:
-      "https://www.saarbruecker-zeitung.de/imgs/03/1/3/8/7/2/3/4/tok_eea62df758d98f24cd14c38116378100/w900_h724_x450_y362_sl-stachurski_spotgun.jpg_1970-G9O56QFDE.1-ORG_CCIDIM-106-48112f8d0db68f1a.jpg",
+      "./assets/micha.jpeg",
     hobbies: ["code", "vegan", "master"],
   },
   {
     name: "Melad",
     email: "melad@apollo18.io",
-    image: "https://avatars0.githubusercontent.com/u/55791371?s=400&v=4",
+    image: "./assets/melad.jpeg",
     hobbies: ["code", "math", "music"],
   },
   {
     name: "Kinan",
     email: "kinan@apollo18.io",
-    image: "https://avatars0.githubusercontent.com/u/57760356?s=400&v=4",
+    image: "./assets/kinan.jpeg",
     hobbies: ["code", "sleep", "laugh"],
   },
   {
@@ -68,13 +68,13 @@ const employees = [
   {
     name: "Majd",
     email: "majd@apollo18.io",
-    image: "https://avatars2.githubusercontent.com/u/40263435?s=400&v=4",
+    image: "./assets/majd.jpeg",
     hobbies: ["code", "game", "done"],
   },
   {
     name: "Gaël",
     email: "gael@apollo18.io",
-    image: "https://avatars2.githubusercontent.com/u/55788820?s=460&v=4",
+    image: "./assets/gael.jpeg",
     hobbies: ["code", "ask", "design"],
   },
 ];
